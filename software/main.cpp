705a411299908f3fba790def9796ac2b21842aa8
cat40 <19844790+cat40@users.noreply.github.com>
2025-08-16 05:08:46 -0400

    primative blink program

--- software/main.cpp
+++ software/main.cpp
@@ -1,6 +1,10 @@
 #include "pico/stdlib.h"
 
+extern "C"
+{
 #include "config.h"
+#include "utilities.h"
+}
 
 #include <stdio.h>
 
@@ -12,7 +16,17 @@ int main(void)
 
     busy_wait_ms(STARTUP_WAIT);
 
-    printf("Starting program Pokey, hash: %s\n", COMMIT_HASH);
+    printf("Starting program chore-board, hash: %s\n", COMMIT_HASH);
+
+    setup_digital_output(FUNSIES_LED_PIN, 0);
+
+    while (1)
+    {
+        busy_wait_ms(1000);
+        gpio_put(FUNSIES_LED_PIN, 1);
+        busy_wait_ms(1000);
+        gpio_put(FUNSIES_LED_PIN, 0);
+    }
 
     while(1);
 }
