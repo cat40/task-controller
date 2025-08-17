460d91ee25673499257829e986eb439110bebe54
cat40 <19844790+cat40@users.noreply.github.com>
2025-08-17 00:16:04 -0400

    software stuff

--- software/main.cpp
+++ software/main.cpp
@@ -7,8 +7,16 @@ extern "C"
 }
 
 #include <stdio.h>
+#include <stdint.h>
 
 #include "Adafruit_NeoPixel.hpp"
+#include "button_matrix.hpp"
+
+Adafruit_NeoPixel rgb1(DEFAULT_NUM_PIXELS, RGB_1_PIN, PIXEL_PARAMETERS);
+Adafruit_NeoPixel rgb2(DEFAULT_NUM_PIXELS, RGB_2_PIN, PIXEL_PARAMETERS);
+Adafruit_NeoPixel rgb3(DEFAULT_NUM_PIXELS, RGB_3_PIN, PIXEL_PARAMETERS);
+
+ButtonMatrix buttons(MATRIX_ROWS, MATRIX_COLUMNS, NUM_MATRIX_ROWS NUM_MATRIX_COLUMNS, 200)
 
 int main(void)
 {
@@ -18,14 +26,26 @@ int main(void)
 
     printf("Starting program chore-board, hash: %s\n", COMMIT_HASH);
 
-    setup_digital_output(FUNSIES_LED_PIN, 0);
+    setup_digital_output(FUNSIES_LED_PIN, 1);
+
+    buttons.init();
+
+    rgb1.updateLength(30);
+
+    uint32_t hue = 0;
 
     while (1)
     {
-        busy_wait_ms(1000);
-        gpio_put(FUNSIES_LED_PIN, 1);
-        busy_wait_ms(1000);
-        gpio_put(FUNSIES_LED_PIN, 0);
+        // busy_wait_ms(1000);
+        // gpio_put(FUNSIES_LED_PIN, 1);
+        // busy_wait_ms(1000);
+        // gpio_put(FUNSIES_LED_PIN, 0);
+        rgb1.fill(rgb1.ColorHSV(hue));
+        rgb1.setBrightness(0x0f);
+        rgb1.show();
+        busy_wait_ms(25);
+        hue += 256;
+        printf("color: %u\n", hue);
     }
 
     while(1);
